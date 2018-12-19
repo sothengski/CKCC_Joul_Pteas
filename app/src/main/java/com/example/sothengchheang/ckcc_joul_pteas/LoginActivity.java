a20a2cce9cc00055084ee8cdc2201093689f8327
ASUS <sysreyteav@gmail.com>
2018-12-19 15:17:44 +0700

    Faebook login Part

--- app/src/main/java/com/example/sothengchheang/ckcc_joul_pteas/LoginActivity.java
+++ app/src/main/java/com/example/sothengchheang/ckcc_joul_pteas/LoginActivity.java
@@ -1,27 +1,92 @@
 package com.example.sothengchheang.ckcc_joul_pteas;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements FacebookCallback<LoginResult> {
+
+    private CallbackManager callbackManager;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //check login status with facebook
-        if(AccessToken.getCurrentAccessToken() != null){
-            Intent intent = new Intent(this,HomeFragment.class);
+
+       /* // Check login status with Facebook
+        if (AccessToken.getCurrentAccessToken() != null) {
+            Intent intent = new Intent(this, DrawerActivity.class);
+            startActivity(intent);
+            finish();
+            return;
+        }*/
+
+        // Check login status with Firebase
+        if (FirebaseAuth.getInstance().getCurrentUser() != null) {
+            Intent intent = new Intent(this, DrawerActivity.class);
             startActivity(intent);
             finish();
             return;
         }
+
         setContentView(R.layout.signin_signup);
 
         LoginButton btnFacebookLogin = findViewById(R.id.login_fb_button);
+        btnFacebookLogin.setReadPermissions("email", "user_birthday", "user_gender", "user_location");
+
+        callbackManager = CallbackManager.Factory.create();
+        btnFacebookLogin.registerCallback(callbackManager, this);
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onSuccess(LoginResult loginResult) {
+        Log.d("ckcc", "Login with Facebook success.");
+        // Pass token to Firebase Auth to manage
+        AuthCredential credential = FacebookAuthProvider.getCredential(loginResult.getAccessToken().getToken());
+        FirebaseAuth.getInstance().signInWithCredential(credential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Log.d("ckcc", "Login with Firebase success.");
+                    Intent intent = new Intent(LoginActivity.this, DrawerActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    Log.d("ckcc", "Login with Firebase error: " + task.getException());
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onCancel() {
+        Log.d("ckcc", "Login cancel.");
+    }
+
+    @Override
+    public void onError(FacebookException error) {
+        Log.d("ckcc", "Login error: " + error.getMessage());
+    }
 }
--- app/src/main/java/com/example/sothengchheang/ckcc_joul_pteas/ProfileFragment.java
+++ app/src/main/java/com/example/sothengchheang/ckcc_joul_pteas/ProfileFragment.java
@@ -1,21 +1,23 @@
 package com.example.sothengchheang.ckcc_joul_pteas;
 
-
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.facebook.AccessToken;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
 import com.facebook.drawee.view.SimpleDraweeView;
+import com.google.gson.Gson;
 
-import org.w3c.dom.Text;
+import org.json.JSONObject;
 
-
-/**
- * A simple {@link Fragment} subclass.
- */
 public class ProfileFragment extends Fragment {
 
     private SimpleDraweeView imgProfile;
@@ -23,20 +25,63 @@ public class ProfileFragment extends Fragment {
     private TextView txtEmail;
     private TextView txtGender;
     private TextView txtBirthday;
-    private TextView txtAdress;
+    private TextView txtAddress;
 
-
-    public ProfileFragment() {
-        // Required empty public constructor
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View home = inflater.inflate(R.layout.fragment_profile, container, false);
+        return home;
     }
 
-
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View home = inflater.inflate(R.layout.fragment_profile, container,false);
-        return home;
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        imgProfile = view.findViewById(R.id.img_profile);
+        txtName = view.findViewById(R.id.txtName);
+        txtEmail = view.findViewById(R.id.txtEmail);
+        txtGender = view.findViewById(R.id.txtGender);
+        txtBirthday = view.findViewById(R.id.txtBirthday);
+        txtAddress = view.findViewById(R.id.txtAddress);
+
+        loadProfileInfo();
+
+    }
+
+    private void loadProfileInfo() {
+        Log.d("ckcc", "loadProfileInfo");
+        AccessToken accessToken = AccessToken.getCurrentAccessToken();
+        GraphRequest request = GraphRequest.newMeRequest(accessToken,
+                new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted(JSONObject object, GraphResponse response) {
+                        fetchResult(object);
+                    }
+                });
+
+        Bundle parameters = new Bundle();
+        parameters.putString("fields", "name,email,birthday,gender,location");
+        parameters.putString("field", "id");
+        request.setParameters(parameters);
+        request.executeAsync();
+    }
+
+    private void fetchResult(JSONObject object) {
+        Log.d("ckcc", "fetchResult: " + object);
+
+        Gson gson = new Gson();
+        UserProfile profile = gson.fromJson(object.toString(), UserProfile.class);
+        txtName.setText("Name: " + profile.name);
+        txtEmail.setText("Email: " + profile.email);
+        txtGender.setText("Gender: " + profile.gender);
+        txtBirthday.setText("Birthday: " + profile.birthday);
+        txtAddress.setText("Address: " + profile.location.name);
+
+        String profileImageUrl = "http://graph.facebook.com/" + profile.id + "/picture?type=large";
+        imgProfile.setImageURI(profileImageUrl);
+
+
     }
 
 }
