45e95862a295b6bc61743158d27a71764dc6df07
ASUS <sysreyteav@gmail.com>
2018-12-19 15:34:20 +0700

    Faebook login user profilw

---
+++ app/src/main/java/com/example/sothengchheang/ckcc_joul_pteas/UserProfile.java
@@ -0,0 +1,18 @@
+package com.example.sothengchheang.ckcc_joul_pteas;
+
+public class UserProfile {
+
+    public String id;
+    public String name;
+    public String email;
+    public String gender;
+    public String birthday;
+    public Location location;
+
+    class Location {
+        String id;
+        String name;
+    }
+
+
+}
